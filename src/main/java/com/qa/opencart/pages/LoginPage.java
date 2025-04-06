96acc470e9b8a0584cdcbe50d89e5a82d1ebe226
Dhaval-Devangada <dhaval.devangada@outlook.com>
2025-04-06 22:00:22 +0530

    [Updated] Locator

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -13,7 +13,7 @@ public class LoginPage {
 
     // 1. private By locators - page locators
     private By emailId = By.id("input-email");
-    private By password = By.id("input-passwordddd");
+    private By password = By.id("input-password");
     private By loginBtn = By.xpath("//input[@value='Login']");
     private By forgotPwdLink = By.linkText("Forgotten Password");
     private By registerLink = By.linkText("Register");
